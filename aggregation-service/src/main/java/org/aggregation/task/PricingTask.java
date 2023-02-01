0425d21dd39e4a5c93be41b9bc5eb77e796101e4
kishore dubey <kishore.dubey84@gmail.com>
2023-02-02 01:25:40 +0800

    change

--- aggregation-service/src/main/java/org/aggregation/task/PricingTask.java
+++ aggregation-service/src/main/java/org/aggregation/task/PricingTask.java
@@ -19,18 +19,18 @@ public class PricingTask {
         this.backendClient = backendClient;
     }
 
-    public @NonNull Map<String, String> submit(final List<String> orders) {
+    public @NonNull Map<String, String> submit(final List<String> countryCodes) {
         Map<String, String> response = new HashMap<>();
-        if (orders == null || orders.isEmpty()) return response;
+        if (countryCodes == null || countryCodes.isEmpty()) return response;
 
-        CountDownLatch latch = new CountDownLatch(orders.size());
-        ExecutorService executor = Executors.newFixedThreadPool(orders.size());
-        for (String order : orders) {
+        CountDownLatch latch = new CountDownLatch(countryCodes.size());
+        ExecutorService executor = Executors.newFixedThreadPool(countryCodes.size());
+        for (String code : countryCodes) {
             executor.execute(() -> {
                 // code to be executed asynchronously
-                String price = backendClient.getPricing(order);
+                String price = backendClient.getPricing(code);
                 if (price != null) {
-                    response.put(order, price);
+                    response.put(code, price);
                 }
                 latch.countDown();
             });
