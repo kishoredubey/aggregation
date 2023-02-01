25edc83850413190c354f6196c1494d1c7992dd6
kishore dubey <kishore.dubey84@gmail.com>
2023-02-01 23:48:43 +0800

    change

--- aggregation-service/src/main/java/org/aggregation/constants/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package org.aggregation.constants;
-
-public class AppConstants {
-    public static final String ORDER_SHIPMENT_SERVICE = "shipment-products";
-    public static final String TRACK_STATUS_SERVICE = "track-status";
-    public static final String PRICE_SERVICE = "pricing";
-}
