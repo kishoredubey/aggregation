3d4bc83496c4ef9582f635562e34f4eea7d27534
Surendra Mishra (OSV) <surendra.mishra.osv@fedex.com>
2023-02-01 14:56:45 +0000

    test

--- aggregation-service/src/main/java/org/aggregation/services/IAggregationService.java
+++ aggregation-service/src/main/java/org/aggregation/services/IAggregationService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.aggregation.dto.AggregationDto;
 
 public interface IAggregationService {
-
 	AggregationDto getAggregatedProductDetail(List<String> shipmentOrderNumbers, List<String> trackOrderNumbers,
 			List<String> pricingCountryCodes);
-
 }
